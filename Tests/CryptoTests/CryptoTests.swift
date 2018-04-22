0f5be0e366a4aaf2be8beffbfb6721afbdc69254
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2018-04-22 17:34:45 +0300

    Create swift package

---
+++ Tests/CryptoTests/CryptoTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import Crypto
+
+final class CryptoTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Crypto().text, "Hello, World!")
+    }
+
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
